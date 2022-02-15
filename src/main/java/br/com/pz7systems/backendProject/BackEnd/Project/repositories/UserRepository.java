8d25b0db1f5705738eb959f3eaef339c6bd9a757
PardalZ7 <alexandrespecian@gmail.com>
2022-02-15 17:34:20 -0300

    alterando banco para h2

--- src/main/java/br/com/pz7systems/backendProject/BackEnd/Project/repositories/UserRepository.java
+++ src/main/java/br/com/pz7systems/backendProject/BackEnd/Project/repositories/UserRepository.java
@@ -2,11 +2,10 @@ package br.com.pz7systems.backendProject.BackEnd.Project.repositories;
 
 import br.com.pz7systems.backendProject.BackEnd.Project.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
+//@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
